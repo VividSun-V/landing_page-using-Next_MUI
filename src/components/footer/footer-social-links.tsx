e8d6563253580cac8575197522193d9ea4ac3689
VividSun-V <tosifak257@outlook.com>
2022-05-04 00:15:32 +0700

    fix responsive mobile view

--- src/components/footer/footer-social-links.tsx
+++ src/components/footer/footer-social-links.tsx
@@ -20,13 +20,8 @@ export const socialLinks: SocialLink[] = [
     icon: '/images/icons/twitter.svg',
   },
   {
-    name: 'Dribbble',
-    link: 'https://dribbble.com/shots/18114471-Coursespace-Online-Course-Landing-Page',
-    icon: '/images/icons/dribbble.svg',
-  },
-  {
     name: 'Github',
-    link: 'https://github.com/hiriski/coursespace-landing-page',
+    link: 'https://github.com/VividSun-V/coursespace-landing-page',
     icon: '/images/icons/github.svg',
   },
 ]
